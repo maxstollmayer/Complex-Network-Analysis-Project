80ec882de95d3d9a5c1c6869b87506437730d389
Max <max.stolly@gmail.com>
2022-02-09 19:47:33 +0100

    added preview method

--- layout.py
+++ layout.py
@@ -4,28 +4,38 @@ previewing and writing the node and edge tables to files.
 """
 
 from dataclasses import dataclass
-from typing import Any, Dict, Iterable, Optional, Union
+from typing import Any, Collection, Dict, Optional, Tuple, Union
 import networkx as nx
 import numpy as np
 import pandas as pd
+from plotly import graph_objects as go
 
 
 # defaults
 NODE_COLUMNS = ["x", "y", "z", "r", "g", "b", "a", "name"]
 EDGE_COLUMNS = ["i", "j", "r", "g", "b", "a"]
 DEFAULT_2D_Z = 0
-DEFAULT_NODE_COLOR = [31, 119, 180, 100]
-DEFAULT_EDGE_COLOR = [0, 0, 0, 100]
+DEFAULT_NODE_COLOR = (31, 119, 180, 100)
+DEFAULT_EDGE_COLOR = (0, 0, 0, 100)
 DEFAULT_NAME_PREFIX = "node_"
 DEFAULT_NODE_FILE = "nodes.csv"
 DEFAULT_EDGE_FILE = "edges.csv"
 
 # type aliases
+Number = Union[int, float]
 Graph = nx.Graph
-Positions = Dict[Any, Iterable[Union[int, float]]]
-Color = Iterable[int]
-Colors = Iterable[Color]
-Names = Iterable[str]
+Positions = Dict[Any, Collection[Number]]
+Color = Tuple[Number, Number, Number, Number]
+Colors = Collection[Color]
+Names = Collection[str]
+
+
+def normalize_colors(colors: Colors) -> Colors:
+    """Returns normalized RGBA colors in range [0, 1]."""
+    normalized = np.array(colors)
+    normalized[:, -1] = np.where(normalized[:, -1] >= 100, 100, normalized[:, -1])
+    normalized = normalized / np.array([255, 255, 255, 100])
+    return normalized
 
 
 @dataclass
@@ -40,17 +50,36 @@ class Layout:
     node_colors: Optional[Colors] = None
     edge_colors: Optional[Colors] = None
 
+    def __post_init__(self) -> None:
+        """Data validation and assignments of default colors and names where necessary."""
+        n_nodes = self.graph.number_of_nodes()
+        n_edges = self.graph.number_of_edges()
+
+        if len(self.positions) != n_nodes:
+            raise ValueError("Given positions are not compatible with graph.")
+
+        if np.array(list(self.positions.values())).shape[1] not in [2, 3]:
+            raise ValueError("Only 2D and 3D layouts are supported.")
+
+        if self.node_names is None or len(self.node_names) != n_nodes:
+            self.node_names = [
+                DEFAULT_NAME_PREFIX + str(node) for node in self.positions.keys()
+            ]
+
+        if self.node_colors is None or len(self.node_colors) != n_nodes:
+            self.node_colors = self.node_colors = np.repeat(
+                [DEFAULT_NODE_COLOR], repeats=n_nodes, axis=0
+            )
+
+        if self.edge_colors is None or len(self.edge_colors) != n_edges:
+            self.edge_colors = np.repeat(
+                [DEFAULT_EDGE_COLOR], repeats=self.graph.number_of_edges(), axis=0
+            )
+
     def node_table(self) -> pd.DataFrame:
         """
         Returns table with normalized node coordinates, colors and names.
         """
-
-        # sets default node names and colors if not given
-        n_nodes = self.graph.number_of_nodes()
-        if self.node_names is None:
-            self.node_names = [DEFAULT_NAME_PREFIX + str(idx) for idx in range(n_nodes)]
-        if self.node_colors is None:
-            self.node_colors = np.repeat([DEFAULT_NODE_COLOR], repeats=n_nodes, axis=0)
 
         # normalizes coordinates
         coords = np.array(list(self.positions.values()))
@@ -64,33 +93,52 @@ class Layout:
                 [*xy, DEFAULT_2D_Z, *rgba, name]
                 for xy, rgba, name in zip(coords, self.node_colors, self.node_names)
             ]
-        elif dim == 3:
+        else:
             data = [
                 [*xyz, *rgba, name]
                 for xyz, rgba, name in zip(coords, self.node_colors, self.node_names)
             ]
-        else:
-            raise ValueError(
-                f"Only 2D and 3D layouts are supported. Not dimension {dim}."
-            )
 
         return pd.DataFrame.from_records(data, columns=NODE_COLUMNS)
 
     def edge_table(self) -> pd.DataFrame:
         """Returns table with edge data and colors."""
-
-        # sets default edge colors if not given
-        if self.edge_colors is None:
-            self.edge_colors = np.repeat(
-                [DEFAULT_EDGE_COLOR], repeats=self.graph.number_of_edges(), axis=0
-            )
-
         data = [[*ij, *rgba] for ij, rgba in zip(self.graph.edges, self.edge_colors)]
         return pd.DataFrame(data, columns=EDGE_COLUMNS)
 
-    def preview(self) -> None:
+    def preview(self, renderer: Optional[str] = None) -> None:
         """"""
-        pass
+        normalized_node_colors = normalize_colors(self.node_colors)
+        normalized_edge_colors = normalize_colors(self.edge_colors)
+        nodes = self.node_table()
+
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scatter3d(
+                x=nodes["x"],
+                y=nodes["y"],
+                z=nodes["z"],
+                mode="markers",
+                marker=dict(color=normalized_node_colors),
+                text=nodes["name"],
+                name="",
+                showlegend=False,
+            )
+        )
+        for u, v in self.graph.edges:
+            df = nodes.loc[[u, v], :]
+            fig.add_trace(
+                go.Scatter3d(
+                    x=df["x"],
+                    y=df["y"],
+                    z=df["z"],
+                    mode="lines",
+                    showlegend=False,
+                    hoverinfo="skip",
+                    line=dict(color=normalized_edge_colors),
+                )
+            )
+        fig.show(renderer=renderer)
 
     def write(
         self,
@@ -105,9 +153,9 @@ class Layout:
 # only for testing purposes
 def main() -> None:
     """
-    Generates exemplary graph and layout, displays a preview, prints and writes node and edge tables.
+    Generates exemplary graph and layout, displays a preview and prints node and edge tables.
     """
-    G: Graph = nx.erdos_renyi_graph(10, 0.25)
+    G: Graph = nx.erdos_renyi_graph(10, 0.5)
     pos = nx.spring_layout(G, dim=3)
     layout = Layout(G, pos)
     print("Node data:")
@@ -115,7 +163,6 @@ def main() -> None:
     print("\nEdge data:")
     print(layout.edge_table())
     layout.preview()
-    layout.write()
 
 
 if __name__ == "__main__":
